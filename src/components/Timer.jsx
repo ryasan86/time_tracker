9e2df9edb9d6a6e68646a1110cf137bb63090661
Ryan Santos <ryansantos86@gmail.com>
2018-04-29 13:31:58 -0700

    layout complete

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import { renderElapsedString } from './../helpers';
 
 export default class Timer extends Component {
   render() {
-    return <div className="card">Timer</div>;
+    const elapsedString = renderElapsedString(this.props.elapsed);
+    return (
+      <div className="card">
+        <div className="content">
+          <div className="header">{this.props.title}</div>
+          <div className="meta">{this.props.project}</div>
+          <div align="center">
+            <h2>{elapsedString}</h2>
+          </div>
+          <div className="icon-row">
+            <span>&#128465;</span>
+            <span>&#9998;</span>
+          </div>
+        </div>
+        <button className="timer-button green">Start</button>
+      </div>
+    );
   }
 }
--- src/components/TimerForm.jsx
+++ src/components/TimerForm.jsx
@@ -5,20 +5,18 @@ export default class TimerForm extends Component {
     const submitText = this.props.title ? 'Update' : 'Create';
     return (
       <div className="card">
-        <div className="content">
-          <div className="form">
-            <div className="field">
-              <label>Title</label>
-              <input type="text" defaultValue={this.props.title} />
-            </div>
-            <div className="field">
-              <label>Project</label>
-              <input type="text" defaultValue={this.props.project} />
-            </div>
-            <div className="attached" align="center">
-              <button className="blue">{submitText}</button>
-              <button className="red">cancel</button>
-            </div>
+        <div className="form">
+          <div className="field">
+            <label>Title</label>
+            <input type="text" defaultValue={this.props.title} />
+          </div>
+          <div className="field">
+            <label>Project</label>
+            <input type="text" defaultValue={this.props.project} />
+          </div>
+          <div className="attached" align="center">
+            <button className="blue">{submitText}</button>
+            <button className="red">cancel</button>
           </div>
         </div>
       </div>
